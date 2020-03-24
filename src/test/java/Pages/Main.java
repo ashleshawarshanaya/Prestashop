e1fbfee29486af1826b55981b114c7e586f17b2e
ashlesha <washlesha.sandhiri@gmail.com>
2020-03-24 12:48:23 +0100

    Initiate commit

---
+++ src/test/java/Pages/Main.java
@@ -0,0 +1,19 @@
+package Pages;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class Main {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(presta.class);
+        System.out.println("Result Successful: "+result.wasSuccessful());
+        for (Failure failure : result.getFailures())
+        {
+            System.out.println(failure.toString());
+        }
+    }
+
+    }
+
+
